e68702ade9102c8335713ae0380e7ce1b98a5c1d
Jose Tavarez <newyorkcityjose@gmail.com>
2021-08-31 19:31:27 -0400

    computer dealer function

--- script.js
+++ script.js
@@ -32,51 +32,86 @@ function shuffleDeck(shuffle) {
     shuffle[i] = shuffle[random];
   }
 }
-function cardhandle(){
-let fresa = card();
-shuffleDeck(fresa);
-let randomCard = Math.floor(Math.random() * 52)
-
-return fresa[randomCard]
+function shuffleDeckComputer(shufflecard) {
+  for (let i = 0; i < 52; i++) {
+    let random = Math.floor(Math.random() * 52);
+    shufflecard[i] = shufflecard[random];
+  }
 }
-function blackjackCard(cardPoints){
-  if(cardPoints[0] == 1 || cardPoints[0] === 'J' || cardPoints[0] === 'Q' || cardPoints[0] === 'A' || cardPoints[0] === 'K'){
+
+function cardhandlePlayer() {
+  let fresa = card();
+  shuffleDeck(fresa);
+  let randomCard = Math.floor(Math.random() * 52);
+  return fresa[randomCard];
+}
+function cardhandleComputer() {
+  let fresa = card();
+  shuffleDeck(fresa);
+  let randomCard = Math.floor(Math.random() * 52);
+  return fresa[randomCard];
+}
+console.log(cardhandlePlayer())
+console.log(cardhandleComputer())
+
+function blackjackCard(cardPoints) {
+  if (
+    cardPoints[0] == 1 ||
+    cardPoints[0] === 'J' ||
+    cardPoints[0] === 'Q' ||
+    cardPoints[0] === 'A' ||
+    cardPoints[0] === 'K'
+  ) {
     return 10;
-  }else{
+  } else {
     return cardPoints[0];
   }
 }
-let arr = []
-let cardvalue = blackjackCard(cardhandle())
-const playerCard = document.querySelector('.show-player-card')
-const playerPoints = document.querySelector('.show-player-points')
-const computerCard = document.querySelector('.show-computer-card')
-const deal = document.querySelector('.deal-card')
+let arr = [];
+let cardvaluePlayer = blackjackCard(cardhandlePlayer());
+let cardvalueComputer = blackjackCard(cardhandleComputer());
 
+const playerCard = document.querySelector('.show-player-card');
+const playerPoints = document.querySelector('.show-player-points');
+const computerCard = document.querySelector('.show-computer-card');
+const deal = document.querySelector('.deal-card');
 
-deal.addEventListener('click', function(){
+deal.addEventListener('click', function () {
+  cardPlayerDeal();
+  arrSum();
+  let check = arrSum();
+  if (check > 21) {
+    console.log('Over 21');
+  } else {
+    playerPoints.innerHTML = check;
+  }
+});
 
-  cardClick()
-  arrSum()
-})
-
-function cardClick(){
-  let cardPick = cardhandle();
+function cardPlayerDeal() {
+  let cardPick = cardhandlePlayer();
   playerCard.innerHTML = cardPick;
-  // console.log(blackjackCard(cardPick))
-  arr.push(blackjackCard(cardPick))
-  console.log(arr)
+  arr.push(blackjackCard(cardPick));
+  console.log(arr);
   return arr;
 }
-function arrSum(){
-  let sum = arr.reduce((a, b) => {return Number(a) + Number(b)}, 0)
-  console.log(sum)
-
+function cardComputerDeal() {
+  let cardPick = cardhandleComputer();
+  playerCard.innerHTML = cardPick;
+  arr.push(blackjackCard(cardPick));
+  console.log(arr);
+  return arr;
 }
-function displayPoint(){
-  let cardpoint = cardClick()
-  console.log(cardpoint)
-
+function arrSum() {
+  let sum = arr.reduce((a, b) => {
+    return Number(a) + Number(b);
+  }, 0);
+  console.log(sum);
+  return sum;
+}
+function displayPoint() {
+  let playerPoints = cardPlayerDeal();
+  let computerPoints = cardComputerDeal();
+  console.log(playerPoints);
 }
 
-displayPoint()
+displayPoint();
