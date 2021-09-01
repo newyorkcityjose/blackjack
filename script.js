c9e31bfe20d66325dc506f959c11235fcc014b05
Jose Tavarez <newyorkcityjose@gmail.com>
2021-09-01 08:58:43 -0400

    play again button

--- script.js
+++ script.js
@@ -77,48 +77,60 @@ const playerPoints = document.querySelector('.show-player-points');
 const computerCard = document.querySelector('.show-computer-card');
 const deal = document.querySelector('.deal-card');
 const stand = document.querySelector('.stand');
+const gameOver = document.querySelector('.game-over');
 
 let sum = 0;
-let arr = []
-let arrComputer = []
+let arr = [];
+let arrComputer = [];
 let sumComputer = 0;
 deal.addEventListener('click', function () {
-  let cardPlayer = cardhandlePlayer()
-  let cardComputer = cardhandlePlayer()
+  let cardPlayer = cardhandlePlayer();
+  let cardComputer = cardhandlePlayer();
 
+  let playerView = blackjackCard(cardPlayer);
+  let computerView = blackjackCard(cardComputer);
 
-  let playerView = blackjackCard(cardPlayer)
-  let computerView = blackjackCard(cardComputer)
-
-  arrComputer.push(computerView)
-  arr.push(playerView)
-  sum = arrSumPlayer(arr)
-  sumComputer = arrSumPlayer(arrComputer)
+  arrComputer.push(computerView);
+  arr.push(playerView);
+  sum = arrSumPlayer(arr);
+  sumComputer = arrSumPlayer(arrComputer);
   // console.log(arr)
-  console.log(sumComputer)
-  console.log(arrComputer)
+  console.log(sumComputer);
+  console.log(arrComputer);
   if (sum > 21) {
     console.log('Over 21');
     deal.style.display = 'none';
-    playerPoints.innerHTML = "Bust"
-    stand.style.display = 'none'
+    playerPoints.innerHTML = 'Bust';
+    stand.style.display = 'none';
   } else {
     playerPoints.innerHTML = sum;
   }
 });
-
-stand.addEventListener('click', function(){
+gameOver.style.display = 'none';
+stand.addEventListener('click', function () {
   deal.style.display = 'none';
   playerPoints.innerHTML = 'Player score ' + sum;
-  computerCard.innerHTML = 'Computer score '+ sumComputer;
-  if(sum > sumComputer){
-    console.log('you won!')
-    playerCard.innerHTML = "You Win!"
-  }else{
-    console.log('you lose!')
-    playerCard.innerHTML = "You Lose!"
+  computerCard.innerHTML = 'Computer score ' + sumComputer;
+
+  if (sum > sumComputer || sumComputer < 21) {
+    console.log('you won!');
+    playerCard.innerHTML = 'You Win!';
+    gameOver.style.display = 'block';
+    stand.style.display = 'none';
+  } else if (sum === sumComputer) {
+    playerCard.innerHTML = 'Draw';
+    stand.style.display = 'none';
+    gameOver.style.display = 'block';
+  } else {
+    console.log('you lose!');
+    playerCard.innerHTML = 'You Lose!';
+    gameOver.style.display = 'block';
+    stand.style.display = 'none';
   }
-})
+});
+gameOver.addEventListener('click', function () {
+  window.location.reload();
+});
 
 function cardPlayerDeal(cardPick) {
   cardPick = cardhandlePlayer();
@@ -127,7 +139,6 @@ function cardPlayerDeal(cardPick) {
   console.log(arrplayer);
   return arrplayer;
 }
-
 
 function arrSumPlayer(sumArray) {
   return sumArray.reduce((a, b) => {
