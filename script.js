8e4d008b501c5c7896dd29723b977a5de3f65cec
Jose Tavarez <newyorkcityjose@gmail.com>
2021-08-31 21:40:06 -0400

    computer and player score

--- script.js
+++ script.js
@@ -77,8 +77,11 @@ const playerPoints = document.querySelector('.show-player-points');
 const computerCard = document.querySelector('.show-computer-card');
 const deal = document.querySelector('.deal-card');
 const stand = document.querySelector('.stand');
+
+let sum = 0;
 let arr = []
 let arrComputer = []
+let sumComputer = 0;
 deal.addEventListener('click', function () {
   let cardPlayer = cardhandlePlayer()
   let cardComputer = cardhandlePlayer()
@@ -89,19 +92,31 @@ deal.addEventListener('click', function () {
 
   arrComputer.push(computerView)
   arr.push(playerView)
-  let sum = arrSumPlayer(arr)
-  let sumComputer = arrSumPlayer(arrComputer)
-  console.log(sum)
+  sum = arrSumPlayer(arr)
+  sumComputer = arrSumPlayer(arrComputer)
+  // console.log(arr)
+  console.log(sumComputer)
   console.log(arrComputer)
-
   if (sum > 21) {
     console.log('Over 21');
     deal.style.display = 'none';
     playerPoints.innerHTML = "Bust"
+    stand.style.display = 'none'
   } else {
     playerPoints.innerHTML = sum;
   }
 });
+
+stand.addEventListener('click', function(){
+  deal.style.display = 'none';
+  playerPoints.innerHTML = 'Player score ' + sum;
+  computerCard.innerHTML = 'Computer score '+ sumComputer;
+  if(sum > sumComputer){
+    console.log('you won!')
+  }else{
+    console.log('you lose!')
+  }
+})
 
 function cardPlayerDeal(cardPick) {
   cardPick = cardhandlePlayer();
